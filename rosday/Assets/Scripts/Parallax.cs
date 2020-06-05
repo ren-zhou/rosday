46ed1f221f28a95b259b753a87c56fdcc4630736
Roy <ren.zhou@berkeley.edu>
2020-06-04 20:58:19 -0400

    Create parallax script and apply to background.

---
+++ rosday/Assets/Scripts/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Transform camTransform;
+    private Vector3 prevPos;
+
+    [SerializeField] private Vector2 multiplier;
+
+    void Start()
+    {
+        camTransform = Camera.main.transform;
+        prevPos = camTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 delta = camTransform.position - prevPos;
+        transform.position += new Vector3(delta.x * multiplier.x, delta.y * multiplier.y);
+        prevPos = camTransform.position;
+    }
+}

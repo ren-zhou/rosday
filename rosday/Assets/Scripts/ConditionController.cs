62599c8bd700e3172c2366bcb5e4f2a133fea4f2
Roy <ren.zhou@berkeley.edu>
2020-06-09 16:51:41 -0400

    Add dialogue updates (DialogueSet), resize mayor lemon

---
+++ rosday/Assets/Scripts/ConditionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** Controls various states of the game, including NPC dialogue that is dependent on player actions/player state,
+ map conditions, etc. Manages "Conditions," which are each uniquely identified by a number. */
+public class ConditionController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ rosday/Assets/Scripts/DialogueSet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Random;
+
+public class DialogueSet : MonoBehaviour
+{
+    private Queue<string> orderedDialogue;
+
+    private List<string> randomDialogue;
+
+    private int seed;
+
+    private System.Random random;
+
+    void Start()
+    {
+        Random.InitState(seed);
+        random = new System.Random(seed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string nextLine()
+    {
+        if (orderedDialogue.Count > 0)
+        {
+            return orderedDialogue.Dequeue();
+        }
+        int index = random.Next(0, randomDialogue.Count);
+        return randomDialogue[index];
+    }
+}
